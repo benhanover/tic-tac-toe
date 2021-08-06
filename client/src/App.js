d053e20947a8f4d933c7f21e20286a22ef4b4734
Ben Hanover <benhanover4@gmail.com>
2021-08-06 19:37:07 +0300

    basic server set up

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from 'react';
+import { io } from 'socket.io-client';
 
 function App() {
+  const connect = () => {
+    const socket = io('http://localhost:4000/');
+    // socket.on('test', (msg) => {
+    //   console.log(msg);
+    // });
+  };
+  useEffect(() => {
+    connect();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Tic Tac Toe</h1>
+      <p></p>
     </div>
   );
 }
---
+++ server/index.js
@@ -0,0 +1,66 @@
+const express = require('express');
+const app = express();
+const server = require('http').createServer(app);
+const { Server } = require('socket.io');
+const io = new Server(server, {
+  cors: {
+    origin: '*',
+  },
+});
+
+const players = {};
+let rival;
+
+io.on('connection', (socket) => {
+  console.log(socket.id, 'connected');
+  console.log(players);
+  // socket.emit('test', 'testtt');
+  initializePlayers(socket);
+
+  if (getOpponent(socket)) {
+    socket.emit('game begin', {
+      symbol: players[socket.id].symbol,
+    });
+    getOpponent(socket).emit('game begin', {
+      symbol: players[getOpponent(socket).id].symbol,
+    });
+  }
+
+  socket.on('make move', (data) => {
+    if (!getOpponent(socket)) return;
+    socket.emit('move made', data);
+    getOpponent(socket.emit('move made', data));
+  });
+
+  socket.on('disconnect', () => {
+    console.log(socket.id, 'disconnected');
+    if (getOpponent(socket)) {
+      getOpponent(socket).emit('opponent left');
+    }
+  });
+});
+
+///////////////////////////////////////////////////////////////////////
+function initializePlayers(socket) {
+  players[socket.id] = {
+    opponent: rival,
+    symbol: 'X',
+    socket: socket,
+  };
+  if (rival) {
+    players[socket.id].symbol = 'O';
+    players[rival].opponent = socket.id;
+    rival = null;
+  } else {
+    rival = socket.id;
+  }
+}
+// **
+function getOpponent(socket) {
+  if (!players[socket.id].opponent) return;
+  return players[players[socket.id].opponent].socket;
+}
+
+server.listen(4000, () => {
+  console.log('listening on port 4000');
+});
