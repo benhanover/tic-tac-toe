4e1e6aec31c426db1bf487ba8ca564ebdc7835ca
Ben Hanover <benhanover4@gmail.com>
2021-08-07 11:02:48 +0300

    finshed game todo: cleanup

--- client/src/components/Board/Board.js
+++ client/src/components/Board/Board.js
@@ -9,26 +9,30 @@ const Board = () => {
   const [squares, setSquares] = useState(Array(9).fill(null));
   const [symbol, setSymbol] = useState();
   const [xIsNext, setXisNext] = useState(true);
-  const [showRestartButton, setShowRestartButton] = useState(true);
-  const winner = calculateWinner(squares);
+  const [waitForOpponent, setWaitForOpponent] = useState(true);
+  let winner = calculateWinner(squares);
   let status;
   if (winner) {
     status = 'Winner: ' + winner;
-    // setShowRestartButton(true);
   } else {
     status = xIsNext ? 'Next Player: X' : 'Next Player: O';
   }
 
   const handleClick = (i) => {
+    console.log('1');
+    console.log(symbol, 'xisnext:', xIsNext);
     // test that it is your turn
     if ((xIsNext && symbol === 'O') || (!xIsNext && symbol === 'X')) return;
+    console.log('2');
     const tempSquares = squares.slice();
     // case there is a winner or square is already taken
     if (calculateWinner(squares) || squares[i]) return;
+    console.log('3');
     tempSquares[i] = symbol;
     setSquares(tempSquares);
     socket.emit('make move', { squares: tempSquares, xIsNext: !xIsNext });
     setXisNext(!xIsNext);
+    console.log('4');
   };
 
   const renderSquare = (i) => {
@@ -40,12 +44,27 @@ const Board = () => {
     setSocket(socket);
 
     socket.on('move made', (data) => {
+      console.log('move made');
       setSquares(data.squares);
       setXisNext(data.xIsNext);
     });
 
     socket.on('game begin', ({ symbol }) => {
+      console.log('game begin', symbol);
+      winner = null;
+      setWaitForOpponent(false);
+      setXisNext(true);
       setSymbol(symbol);
+    });
+
+    socket.on('restart', () => {
+      setSquares(Array(9).fill(null));
+      winner = null;
+    });
+
+    socket.on('waiting for opponent', () => {
+      setSquares(Array(9).fill(null));
+      setWaitForOpponent(true);
     });
     // socket.on('test', (msg) => {
     //   console.log(msg);
@@ -57,30 +76,35 @@ const Board = () => {
 
   return (
     <div>
-      <div className='status'>{status}</div>
-      <div className='board-row'>
-        {renderSquare(0)}
-        {renderSquare(1)}
-        {renderSquare(2)}
-      </div>
-      <div className='board-row'>
-        {renderSquare(3)}
-        {renderSquare(4)}
-        {renderSquare(5)}
-      </div>
-      <div className='board-row'>
-        {renderSquare(6)}
-        {renderSquare(7)}
-        {renderSquare(8)}
-      </div>
-      {showRestartButton && (
-        <button
-          onClick={() => {
-            socket.emit('restart');
-          }}
-        >
-          Restart
-        </button>
+      {waitForOpponent ? (
+        <h1> Waiting for player to join</h1>
+      ) : (
+        <>
+          <div>you are: {symbol}</div>
+          <div className='status'>{status}</div>
+          <div className='board-row'>
+            {renderSquare(0)}
+            {renderSquare(1)}
+            {renderSquare(2)}
+          </div>
+          <div className='board-row'>
+            {renderSquare(3)}
+            {renderSquare(4)}
+            {renderSquare(5)}
+          </div>
+          <div className='board-row'>
+            {renderSquare(6)}
+            {renderSquare(7)}
+            {renderSquare(8)}
+          </div>
+          <button
+            onClick={() => {
+              socket.emit('restart');
+            }}
+          >
+            Restart
+          </button>
+        </>
       )}
     </div>
   );
--- server/index.js
+++ server/index.js
@@ -15,6 +15,7 @@ io.on('connection', (socket) => {
   console.log(socket.id, 'connected');
   // socket.emit('test', 'testtt');
   initializePlayers(socket);
+  console.log(Object.keys(players));
 
   if (getOpponent(socket)) {
     socket.emit('game begin', {
@@ -23,19 +24,50 @@ io.on('connection', (socket) => {
     getOpponent(socket).emit('game begin', {
       symbol: players[getOpponent(socket).id].symbol,
     });
+  } else {
+    socket.emit('waiting for opponent');
   }
 
   socket.on('make move', (data) => {
     if (!getOpponent(socket)) return;
+    console.log(socket.id);
+    console.log(getOpponent(socket).id);
     socket.emit('move made', data);
     getOpponent(socket).emit('move made', data);
   });
 
+  socket.on('restart', () => {
+    socket.emit('restart');
+    getOpponent(socket).emit('restart');
+  });
+
   socket.on('disconnect', () => {
     console.log(socket.id, 'disconnected');
-    if (getOpponent(socket)) {
-      getOpponent(socket).emit('opponent left');
+    const opponent = getOpponent(socket);
+    if (opponent) {
+      opponent.emit('waiting for opponent');
+      if (rival) {
+        players[rival].symbol = 'X';
+        players[opponent.id].symbol = 'O';
+        players[rival].opponent = opponent.id;
+        players[opponent.id].opponent = rival;
+        players[rival].socket.emit('game begin', {
+          symbol: players[rival].symbol,
+        });
+        opponent.emit('game begin', {
+          symbol: players[opponent.id].symbol,
+        });
+        rival = null;
+      } else {
+        players[opponent.id].opponent = undefined;
+        players[opponent.id].symbol = 'X';
+        rival = opponent.id;
+      }
+    } else {
+      rival = null;
     }
+    delete players[socket.id];
+    // console.log(Object.keys(players));
   });
 });
 
